e956b63deacce37198dfe9cab9b5f91e1b8692ee
wei chen <weichen9083@gmail.com>
2019-11-27 21:36:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
 class Song 
   
-  attr_accessor :artist 
+  attr_accessor :artist, :title
   
-  def intiallize()
-  
+  def intiallize(title, artist)
+    @title = title
+    @artist = artist 
+end 
 end 
